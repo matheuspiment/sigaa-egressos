38a6169daaf1bbecc65b7a0d861009529a4b0603
matheuspiment <matheuspiment@hotmail.com>
2018-07-02 21:22:35 -0300

    fix(checkMessage): Verificar se os valores do objeto message para publicação estão com o formato/tip
    fix #2

---
+++ src/index.test.js
@@ -0,0 +1,28 @@
+import { checkMessage } from './index';
+
+describe('sigaa-egressos', () => {
+  describe('checkMessage', () => {
+    test('should be a invalid message: must be an object', () => {
+      expect(() => checkMessage(5)).toThrowError('The message must be an object');
+    });
+    test('should be a invalid message: does not have all the expected keys', () => {
+      expect(() => checkMessage({ id: 2 })).toThrowError('The message does not have all the expected keys');
+    });
+    test('should be a invalid message: contains invalid keys', () => {
+      expect(() => checkMessage({
+        id: 1,
+        title: 'Teste',
+        type: 'noticia',
+        testo: 'Olá',
+      })).toThrowError('The message contains invalid keys');
+    });
+    test('should be a invalid message: wrong value for keys', () => {
+      expect(() => checkMessage({
+        id: 1,
+        title: 3,
+        type: 'noticia',
+        description: 'Olá',
+      })).toThrowError('The value for the title must be a string');
+    });
+  });
+});
