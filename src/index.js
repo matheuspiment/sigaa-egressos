7b621c06518002966eddedf0581573a632e7c141
Matheus Ribeiro Pimenta Nunes <matheuspiment@hotmail.com>
2020-10-31 00:46:11 -0300

    feat: Implementar a nova API
    BREAKING CHANGE: As credencias para o broker agora devem ser fornecidas.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
 import * as emitter from 'emitter-io';
 import { hasClient, checkMessage } from './utils';
 
-// Env for tests
-const EMITTER_CHANNEL = 'news';
-const EMITTER_CHANNEL_KEY = 'Yr2-O0G1QT3TQ70aPIwFeQk264wLw02n';
-
 /**
  * An Emitter instance.
  * @global
@@ -32,56 +28,101 @@ const disconnect = () => {
 
 /**
  * Subscribes to the channel.
+ *
  * @function
+ *
+ * @param {string} [credentials.key] Channel key.
+ * @param {string} [credentials.channel] Channel name.
  * @param {number} [last] Define how many stored messages you want to retrieve.
- * @throws {Error} The parameter last must be a number.
+ *
+ * @throws {TypeError} `credentials.key` must be a string.
+ * @throws {TypeError} `credentials.channel` must be a string.
+ * @throws {TypeError} `last` must be a number.
  */
-const subscribe = (last) => {
+const subscribe = (credentials, last) => {
+  if (credentials && typeof credentials.key !== 'string') {
+    throw new TypeError('The parameter credentials.key must be a string');
+  }
+
+  if (credentials && typeof credentials.channel !== 'string') {
+    throw new TypeError('The parameter credentials.channel must be a string');
+  }
+
   if (last !== undefined && typeof last !== 'number') {
-    throw new Error('The parameter last must be a number');
+    throw new TypeError('The parameter last must be a number');
   }
 
   hasClient(client);
 
   client.subscribe({
-    key: EMITTER_CHANNEL_KEY,
-    channel: EMITTER_CHANNEL,
+    key: credentials.key,
+    channel: credentials.channel,
     last: last === undefined ? 0 : last,
   });
 };
 
 /**
  * Unsubscribes to the channel.
+ *
  * @function
+ *
+ * @param {string} [credentials.key] Channel key.
+ * @param {string} [credentials.channel] Channel name.
+ *
+ * @throws {TypeError} `credentials.key` must be a string.
+ * @throws {TypeError} `credentials.channel` must be a string.
  */
-const unsubscribe = () => {
+const unsubscribe = (credentials) => {
+  if (credentials && typeof credentials.key !== 'string') {
+    throw new TypeError('The parameter credentials.key must be a string');
+  }
+
+  if (credentials && typeof credentials.channel !== 'string') {
+    throw new TypeError('The parameter credentials.channel must be a string');
+  }
+
   hasClient(client);
 
   client.unsubscribe({
-    key: EMITTER_CHANNEL_KEY,
-    channel: EMITTER_CHANNEL,
+    key: credentials.key,
+    channel: credentials.channel,
   });
 };
 
 /**
  * Publish a message to the channel.
+ *
  * @function
+ *
+ * @param {string} [credentials.key] Channel key.
+ * @param {string} [credentials.channel] Channel name.
  * @param {!object} message A javascript object to publish.
  * @param {number} [ttl] Is the time-to-live of message, in seconds.
- * @throws {Error} The ttl parameter must be a number.
+ *
+ * @throws {TypeError} `credentials.key` must be a string.
+ * @throws {TypeError} `credentials.channel` must be a string.
+ * @throws {TypeError} `last` must be a number.
  */
-const publish = (message, ttl) => {
+const publish = (credentials, message, ttl) => {
+  if (credentials && typeof credentials.key !== 'string') {
+    throw new TypeError('The parameter credentials.key must be a string');
+  }
+
+  if (credentials && typeof credentials.channel !== 'string') {
+    throw new TypeError('The parameter credentials.channel must be a string');
+  }
+
   checkMessage(message);
 
   if (ttl !== undefined && typeof ttl !== 'number') {
-    throw new Error('The ttl parameter must be a number');
+    throw new TypeError('The ttl parameter must be a number');
   }
 
   hasClient(client);
 
   client.publish({
-    key: EMITTER_CHANNEL_KEY,
-    channel: EMITTER_CHANNEL,
+    key: credentials.key,
+    channel: credentials.channel,
     message: JSON.stringify(message),
     ttl: ttl === undefined ? 0 : ttl,
   });
@@ -89,18 +130,16 @@ const publish = (message, ttl) => {
 
 /**
  * Define the callback for received message packet.
+ *
  * @function
+ *
  * @param {onMessageCallback} callback
- * @throws {Error} A callback function must be provided.
- * @throws {Error} The parameter is not a function.
+ *
+ * @throws {TypeError} A callback function must be provided.
  */
 const onMessage = (callback) => {
-  if (callback === undefined) {
-    throw new Error('A function must be provided');
-  }
-
   if (typeof callback !== 'function') {
-    throw new Error('The parameter is not a function');
+    throw new TypeError('A function must be provided');
   }
 
   hasClient(client);
